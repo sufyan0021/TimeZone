48fde017b825b12ed54381f3a7e95da20f98514d
Sufyan Mahmood <Sufyan_Mahmood@epam.com>
2021-02-05 00:06:39 +0530

    removed unused code

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,4 +1,3 @@
-import { getDefaultNormalizer } from '@testing-library/react'
 import React,{useState,useEffect} from 'react'
 import getTime from '../utils/getTime';
 
--- src/components/TimeZone.test.js
+++ src/components/TimeZone.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import {render,fireEvent} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import TimeZone from './TimeZone';
-import getData from '../utils/getData';
-import Time from './Time';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 
+
+//test to check if dropdown button renders
 it('renders correctly',()=>{
     const {queryByTestId} = render(<TimeZone/>);
 
     expect(queryByTestId('dropdown-timezone')).toBeTruthy();
     
 })
+
 
 
 
